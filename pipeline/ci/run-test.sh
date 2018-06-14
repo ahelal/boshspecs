a237246036429d017af942298d8b9813c7b62301
Markus Bukowski <mbukowski@pivotal.io>
2018-06-14 15:20:03 +0100

    Task creates symlink to git repo so that GOPATH works.

--- pipeline/ci/run-test.sh
+++ pipeline/ci/run-test.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eu
+
+mkdir -p /go/src/github.com/ahelal/ || echo "OK" 
+
+ln -fs $(pwd) /go/src/github.com/ahelal/boshspecs/
+pushd /go/src/github.com/ahelal/boshspecs
 
 make clean
 make deps
 make test
+
+popd
