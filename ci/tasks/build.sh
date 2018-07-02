664aa36b2dd63ab8fed194d14167d2b72c0b82f8
Adham <ahelal@users.noreply.github.com>
2018-07-02 19:47:26 +0200

    build tasks

---
+++ ci/tasks/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+
+export PATH=$GOPATH/bin:$PATH
+
+mkdir -p /go/src/github.com/ahelal/ || echo "OK" 
+
+ln -fs $(pwd) /go/src/github.com/ahelal/boshspecs
+pushd /go/src/github.com/ahelal/boshspecs
+
+# hack to support caching. find a better away
+VENDOR_CONCOURSE="$(pwd)/../vendor-concourse"
+cp -r "${VENDOR_CONCOURSE}"/. vendor/
+make deps
+cp -r vendor/. "${VENDOR_CONCOURSE}"
+
+export BUILD_ONLY_DEFAULT=""
+make build
+
+popd
+
+cp dist/boshspecs ../boshspecs-build
