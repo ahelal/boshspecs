02e350267a8868990c16fe6f079228b251ea6301
Adham <ahelal@users.noreply.github.com>
2018-07-03 21:19:08 +0200

    build only default

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -6,7 +6,7 @@ export PATH=$GOPATH/bin:$PATH
 
 mkdir -p /go/src/github.com/ahelal/ || echo "OK" 
 
-ln -fs $(pwd) /go/src/github.com/ahelal/boshspecs
+ln -fs "$(pwd)" /go/src/github.com/ahelal/boshspecs
 pushd /go/src/github.com/ahelal/boshspecs
 
 # hack to support caching. find a better away
@@ -15,7 +15,7 @@ cp -r "${VENDOR_CONCOURSE}"/. vendor/
 make deps
 cp -r vendor/. "${VENDOR_CONCOURSE}"
 
-export BUILD_ONLY_DEFAULT=""
+export BUILD_ONLY_DEFAULT="YES"
 make build
 
 popd
